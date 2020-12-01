c1413020341984c2d642d7d3c9419f830b482e6c
Yongqi Feng <fyqnankai@163.com>
2020-12-01 23:05:17 +0800

    add pkg in vendor

--- controllers/shpa_controller.go
+++ controllers/shpa_controller.go
@@ -18,15 +18,86 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"math"
+	"time"
 
-	"github.com/go-logr/logr"
+	"github.com/golang/glog"
+
+
+	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv2 "k8s.io/api/autoscaling/v2beta1"
+	v1 "k8s.io/api/core/v1"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/kubernetes/pkg/controller/podautoscaler/metrics"
+	resourceclient "k8s.io/metrics/pkg/client/clientset/versioned/typed/metrics/v1beta1"
+	"k8s.io/metrics/pkg/client/custom_metrics"
+	"k8s.io/metrics/pkg/client/external_metrics"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	webappv1 "sidecar-hpa/api/v1"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
+const (
+	defaultSyncPeriod                            = time.Second * 15
+	defaultTargetCPUUtilizationPercentage  int32 = 80
+	defaultTolerance                             = 0.1
+	defaultDownscaleForbiddenWindowSeconds       = 300
+	defaultUpscaleForbiddenWindowSeconds         = 300
+	defaultScaleUpLimitMinimum                   = 4.0
+	defaultScaleUpLimitFactor                    = 2.0
+)
+
+// Add creates a new CHPA Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	clientConfig := mgr.GetConfig()
+	metricsClient := metrics.NewRESTMetricsClient(
+		resourceclient.NewForConfigOrDie(clientConfig),
+		custom_metrics.NewForConfigOrDie(clientConfig),
+		external_metrics.NewForConfigOrDie(clientConfig),
+	)
+	clientSet, err := kubernetes.NewForConfig(clientConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	evtNamespacer := clientSet.CoreV1()
+	broadcaster := record.NewBroadcaster()
+	broadcaster.StartLogging(glog.Infof)
+	broadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: evtNamespacer.Events("")})
+	recorder := broadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: "horizontal-pod-autoscaler"})
+
+	replicaCalc := NewReplicaCalculator(metricsClient, clientSet.CoreV1(), defaultTolerance)
+	return &ReconcileCHPA{
+		Client:        mgr.GetClient(),
+		scheme:        mgr.GetScheme(),
+		clientSet:     clientSet,
+		replicaCalc:   replicaCalc,
+		eventRecorder: recorder,
+		syncPeriod:    defaultSyncPeriod,
+	}
+}
 // SHPAReconciler reconciles a SHPA object
 type SHPAReconciler struct {
 	client.Client
--- go.mod
+++ go.mod
@@ -1,14 +1,48 @@
 module sidecar-hpa
 
-go 1.13
+go 1.15
 
 require (
-	github.com/go-logr/logr v0.1.0
+	github.com/go-logr/logr v0.2.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/onsi/ginkgo v1.11.0
 	github.com/onsi/gomega v1.8.1
-	k8s.io/api v0.17.2
-	k8s.io/apimachinery v0.18.8
-	k8s.io/client-go v0.17.2
+	k8s.io/api v0.19.4
+	k8s.io/apimachinery v0.19.4
+	k8s.io/apiserver v0.19.4 // indirect
+	k8s.io/client-go v0.19.4
+	k8s.io/kubernetes v1.19.4
+	k8s.io/metrics v0.19.4
 	sigs.k8s.io/controller-runtime v0.5.0
 	sigs.k8s.io/kind v0.9.0 // indirect
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.0.0-20201114085527-4a626d306b98
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.0.0-20201114091224-a7ee1efe41fc
+	k8s.io/apimachinery => k8s.io/apimachinery v0.20.0-beta.2.0.20201118005411-2456ebdaba22
+	k8s.io/apiserver => k8s.io/apiserver v0.0.0-20201120050325-0e46f0ea2bdc
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.0.0-20201114091436-bb334014705f
+	k8s.io/client-go => k8s.io/client-go v0.0.0-20201121005859-fb61a7c88cb9
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.0.0-20201114092026-235e676f444a
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.0.0-20201114092228-614b98eee358
+	k8s.io/code-generator => k8s.io/code-generator v0.20.0-beta.2.0.20201118094405-356aa54a63d2
+	k8s.io/component-base => k8s.io/component-base v0.0.0-20201114090208-1e84b325f5ba
+	k8s.io/component-helpers => k8s.io/component-helpers v0.20.0-alpha.2.0.20201114090304-7cb42b694587
+	k8s.io/controller-manager => k8s.io/controller-manager v0.20.0-alpha.1.0.20201114091934-20fa1a1257aa
+	k8s.io/cri-api => k8s.io/cri-api v0.20.0-beta.2
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.0.0-20201114092327-833303372de1
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.0.0-20201126170540-6c47de442a82
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.0.0-20201114092129-18c28a4120de
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.0.0-20201114091637-deb12d4b202f
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.0.0-20201114091838-0f62d3991af1
+	k8s.io/kubectl => k8s.io/kubectl v0.0.0-20201114092718-b155278f1f4a
+	k8s.io/kubelet => k8s.io/kubelet v0.0.0-20201114091737-92ded5ee6b96
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.0.0-20201125092503-cc0a0abf3d78
+	k8s.io/metrics => k8s.io/metrics v0.0.0-20201114091333-d70c0e0c6aa5
+	k8s.io/mount-utils => k8s.io/mount-utils v0.20.0-beta.2
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.0.0-20201114090814-1f4e6a92d4b8
+	k8s.io/sample-cli-plugin => k8s.io/sample-cli-plugin v0.0.0-20201114091536-307712dacc73
+	k8s.io/sample-controller => k8s.io/sample-controller v0.0.0-20201114091033-7644cdf6adcd
+
 )
